a04ac6bb170ee52bdaa19d0f4c06d01ec892b65b
jjjssshhh <wjstlgjs_21_@naver.com>
2026-06-05 10:55:57 +0900

    k

--- job03_concat_data.py
+++ job03_concat_data.py
@@ -1,15 +1,15 @@
 import pandas as pd
 df = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section.csv')
-# print(df.head())
-#
-df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section_p.csv')
-df = pd.concat([df, df_temp])
+print(df.head())
+
+# df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section_p.csv')
+# df = pd.concat([df, df_temp])
 
 df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_headline_news_Culture_20260604.csv')
 df = pd.concat([df, df_temp])
 
-df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section.csv')
-df = pd.concat([df, df_temp])
+# df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section.csv')
+# df = pd.concat([df, df_temp])
 
 df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_section_social.csv')
 df = pd.concat([df, df_temp])
@@ -17,11 +17,11 @@ df = pd.concat([df, df_temp])
 df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_world_20260604.csv')
 df = pd.concat([df, df_temp])
 
-df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_headline_news_20260604')
+df_temp = pd.read_csv('/home/jsh-laptop/PycharmProjects/news_category_classification_jsh/data/naver_news_IT_Science_20260604.csv')
 df = pd.concat([df, df_temp]) # 붙이기
 
-print(df.category.value_counts())
-print(df.isnull().sum())
+# print(df.category.value_counts())
+# print(df.isnull().sum())
 
 df = df.drop_duplicates() # 중복제거
 
@@ -31,3 +31,7 @@ print(df.isnull().sum())
 df.info()
 
 df.to_csv('./data/news_titles.csv', index=False)
+
+# 분류는 했는데
+# 문장부호 제거, 형태소로 쪼개야 한다.
+# 불용어 제거
